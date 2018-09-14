fad9d4e4221eaf66364b0b3a035fc924059562ea
dongyiming <yiming.dong@readsense.cn>
2018-09-14 12:33:52 +0800

    commit json type

--- upload-file-ui/src/main/java/com/example/upload_file_ui/service/UploadService.java
+++ upload-file-ui/src/main/java/com/example/upload_file_ui/service/UploadService.java
@@ -2,7 +2,10 @@ package com.example.upload_file_ui.service;
 
 import io.reactivex.Observable;
 import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -25,4 +28,16 @@ public interface UploadService {
     @POST("upload")
     Observable<ResponseBody> uploadFile(@Part MultipartBody.Part file);
 
+    /**
+     * <上传默认json格式内容>
+     *
+     * @param:
+     * @return:
+     * @author: dongyiming
+     * @date: 2018/9/14 11:54
+     * @version: v1.0
+     */
+    @Headers({"Content-Type: application/json", "Accept: application/json"})
+    @POST("upload/file")
+    Observable<ResponseBody> uploadFile(@Body RequestBody request);
 }
